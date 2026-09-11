94f22cd8d44bb9d5b83e0feed951664dd8d0ccfd
Edwin <115915195+whitidevi@users.noreply.github.com>
2026-09-11 18:46:08 +0330

    Add project state throuhg lesson 014

---
+++ wp-content/History/Section 02 - First Coding Steps: PHP/009 PHP Functions/index.php
@@ -0,0 +1,11 @@
+<?php
+function myFirstFunction($name, $color) {
+    echo "<p>Hi, my name is $name and my favorite color is $color</p>";
+}
+
+myFirstFunction('John', 'blue');
+myFirstFunction('Jane', 'green');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
---
+++ wp-content/History/Section 02 - First Coding Steps: PHP/010 PHP Arrays/index.php
@@ -0,0 +1,17 @@
+<?php
+$names = array(
+'Brad',
+'John',
+'Jane',
+'Meowsalot',
+);
+
+
+$count = 0;
+while($count < count($names)) {
+    echo "<li>Hi, my name is $names[$count]. </li>";
+    $count++;
+}
+
+?>
+
