f588b668c8239423e2ff33c9c9a4a90a8b7aca6d
Edwin <115915195+whitidevi@users.noreply.github.com>
2026-09-23 04:14:19 +0330

    S 6-L 2-Displaying Relationships(front end) - Using custom queries

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -21,10 +21,54 @@ while(have_posts()) {
       </div>
 
 	<div class="generic-content"><?php the_content(); ?></div>
-    </div>
-    
 
-	<?php 
+	<?php
+    $today = date('Ymd');
+    //$relatedEvents = new WP_Query(array(
+    $homePageEvents = new WP_Query(array(
+      'post_type' => 'event',
+      'meta_key' => 'event_date',
+      'orderby' => 'meta_value_num',
+      'order' => 'DESC',
+      'meta_query' => array(
+	array(
+	  'key' => 'event_date',
+	  'compare' => '>=',
+	  'value' => $today,
+	  'type' => 'numeric',
+	),
+	array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"'. get_the_ID().'"',
+        ),
+      ),
+    ));
+    while($homePageEvents->have_posts()) {
+                $homePageEvents->the_post(); ?> 
+
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month"><?php
+                $eventDate = new DateTime(get_field('event_date'));
+                echo $eventDate->format('M');
+              ?></span>
+              <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <p><?php if (has_excerpt()) {
+                    echo get_the_excerpt(); //To avoid the vertical gap of "the_excerpt()"
+                } else { 
+                    echo wp_trim_words(get_the_content(), 18);
+                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <?php } wp_reset_postdata(); ?>
+    </div>
+
+
+<?php
 }
 get_footer();
 ?>
