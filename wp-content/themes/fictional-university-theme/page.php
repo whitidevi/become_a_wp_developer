42a2845a4abe63cc07aca4c1cba53a9dd68eccb0
Edwin <115915195+whitidevi@users.noreply.github.com>
2026-09-25 14:17:21 +0330

    S9 Cleaner Code-L1 Reduce Duplication Code_Create our own function.

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -2,26 +2,21 @@
 get_header();
 
 while(have_posts()) {
-        the_post(); ?>
-
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Don't forget to replace me later.</p>
-        </div>
-      </div>
-    </div>
+        the_post();
+	pageBanner(); // We can use an array as input argument for more customization:
+		      // array( 'title' => 'theTitleValue', 
+	 	      //	'subtitle' => 'theSubtitleValue'
+		      // 	'photo' => 'https://wallpapercat.com/w/full/8/f/4/914063-1920x1080-desktop-full-hd-library-wallpaper-photo.jpg',)
+?>
 
     <div class="container container--narrow page-section">
       <?php $theParent = wp_get_post_parent_id(get_the_ID());
 	if ($theParent) { ?>
 	<div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
-        </p>
-      </div>
+          <p>
+            <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+          </p>
+        </div>
 	<?php } ?>
 
 	<?php 
@@ -31,14 +26,14 @@ while(have_posts()) {
 	if ($theParent or $testArray) {
 	?>
       <div class="page-links">
-        <h2 class="page-links__title"><a href="<?php echo get_the_permalink(get_the_ID()); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_the_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
-          
-	 <?php  if ($theParent){
-			$findChildOf = $theParent;
-		} else {
-			$findChildOf = get_the_ID();
-		}
+	 <?php  
+	 if ($theParent){
+	   $findChildOf = $theParent;
+	 } else {
+	   $findChildOf = get_the_ID();
+	 }
 
 	 wp_list_pages(array(
 		'title_li' => NULL,
